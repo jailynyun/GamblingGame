1fdbbf9f89a76b2b8049eff35f2cecebc3f79f9d
Jailyn <jailynyun@gmail.com>
2026-04-01 16:23:00 -0700

    slots fixed

--- gambling-game/Scenes/Games/SlotMachines/slot_machine.gd
+++ gambling-game/Scenes/Games/SlotMachines/slot_machine.gd
@@ -2,6 +2,14 @@ extends Control
 
 @export var n_options: int = 5 
 @export var spinners: Array[Control] 
+
+@onready var spin_button: Button = $SpinButton
+@onready var bet_up_button: Button = $BetUpButton
+@onready var bet_down_button: Button = $BetDownButton
+@onready var credits_label: Label = $CreditsLabel
+@onready var bet_label: Label = $BetLabel
+@onready var result_label: Label = $ResultLabel
+
 var values: Array; var tween: Tween 
 
 func spin():
@@ -30,25 +38,6 @@ func spin():
 	, 0.0, 1.0, 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 
 	tween.tween_callback(func():
-		for s in spinners:
-			s.material.set_shader_parameter("y_offset", offsets[s]["to"])
+		for idx in spinners.size():
+			spinners[idx].material.set_shader_parameter("y_offset", values[idx] * spin_step)
 	)
-#
-#func spin(): 
-	#values = [] 
-	#var spin_step = 1.0 / float(n_options) 
-	#var offsets = {} 
-	#for s in spinners: 
-		#values.append(randi_range(0, n_options - 1)) 
-		#offsets[s] = {'from': s.material.get_shader_parameter('y_offset'), 'to': 3.0 + values[-1] * spin_step } 
-		#
-		#if tween: tween.kill() 
-		#tween = get_tree().create_tween() 
-		#tween.tween_method(func (v): 
-			#for s in spinners: 
-				#s.material.set_shader_parameter('y_offset', lerpf(offsets[s].from, offsets[s].to, v)),
-			#0.0, 1.0, 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC) 
-		#tween.tween_callback(func (): 
-			#for s in spinners:
-				#s.material.set_shader_parameter('y_offset', offsets[s].to))
-			#
