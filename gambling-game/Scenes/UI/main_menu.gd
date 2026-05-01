186eb5a8146a65c9a07fdfa01eb932e02e1471b0
isabelleLi-Projects <iggiesports10@gmail.com>
2026-04-30 21:46:25 -0700

    main menu + pause menu

---
+++ gambling-game/Scenes/UI/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainLobby/Casino.tscn")
+	
+
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+	
