64c499c745bd15038e404b00deba6de3784b60af
Haileyliuu <haileyliuuu@gmail.com>
2026-05-06 21:07:42 -0700

    loan shark cutscene

--- gambling-game/Scenes/MainLobby/casino.gd
+++ gambling-game/Scenes/MainLobby/casino.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 @onready var minigame_ui = $Minigame_UI
+@onready var cutscene = $"Y-sort/LoanSharkCutscene"
+@onready var door_in = $Markers/DoorIn
+@onready var player = $"Y-sort/Player"
+@onready var door_out = $Markers/DoorOut
+
 
 func _ready() -> void:
 	minigame_ui.visible = true
+	cutscene.setup_refs(door_in, player, door_out)
---
+++ gambling-game/Scenes/UI/loan_shark_cutscene.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@onready var shark = $LoanShark
+
+var door_in: Marker2D
+var player: Node2D
+var door_out: Marker2D
+
+func setup_refs(_door_in, _player, _door_out):
+	door_in = _door_in
+	player = _player
+	door_out = _door_out
+
+func _ready() -> void:
+	visible = false
+	GameManager.loanshark_cutscene_started.connect(_start_cutscene)
+
+func _start_cutscene(amount_due: int) -> void:
+	visible = true
+
+	shark.global_position = door_in.global_position
+
+	# move using code instead of AnimationPlayer positions
+	await move_shark(player.global_position, 1.5)
+
+	GameManager.resolve_loanshark_cutscene()
+
+	await get_tree().create_timer(1.5).timeout
+
+	await move_shark(door_out.global_position, 1.5)
+
+	visible = false
+
+func move_shark(target: Vector2, duration: float) -> void:
+	var tween := create_tween()
+	tween.tween_property(shark, "global_position", target, duration)
+	await tween.finished
